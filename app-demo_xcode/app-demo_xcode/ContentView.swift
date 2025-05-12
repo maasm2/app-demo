b5ccc1443e1e1c357db8e36fca4f6f44620fedd8
Ayayaaa12 <103263028+Ayayaaa12@users.noreply.github.com>
2025-05-12 15:35:27 -0400

    new commit

--- app-demo_xcode/app-demo_xcode/ContentView.swift
+++ app-demo_xcode/app-demo_xcode/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -19,6 +20,9 @@ struct ContentView: View {
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
+
