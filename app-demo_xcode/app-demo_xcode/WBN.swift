33b05fed86d9dece47026e551468b0a743559e53
Ayayaaa12 <103263028+Ayayaaa12@users.noreply.github.com>
2025-05-13 11:41:33 -0400

    autorization healthkit

--- app-demo_xcode/app-demo_xcode/WBN.swift
+++ app-demo_xcode/app-demo_xcode/WBN.swift
@@ -17,11 +17,11 @@ struct ContentView: View {
             Text("Welcome to our little Health App")
         }
         .onAppear {
-            askForAutorisation()
+            askForAutorization()
         }
     }
     
-    func askForAutorisation(){
+    func askForAutorization(){
         if(!HKHealthStore.isHealthDataAvailable()){
             print("HealthKit isn't available on this device")
             return
@@ -40,7 +40,7 @@ struct ContentView: View {
                 print("We have access to the weight data yeaaaaaah")
             }
             if let error = error {
-                print("We don't have access to the weight data \(error.localizedDescription)")
+                print("We don't have access to the weight data: \(error.localizedDescription)")
             }
         }
     }
